1e0eeddfd9da00f95f9dcc175ab740a78c7e48b0
dBsooner <daniel@dbsooner.com>
2025-12-14 09:37:30 -0600

    Update WME-URComments-Enhanced.js
    Fix (beta) tag in name and updated release note.

--- WME-URComments-Enhanced.js
+++ WME-URComments-Enhanced.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        WME URComments-Enhanced
+// @name        WME URComments-Enhanced (beta)
 // @namespace   https://greasyfork.org/users/166843
 // @version     2025.12.09.01
 // eslint-disable-next-line max-len
@@ -86,7 +86,7 @@
         _BETA_DL_URL = 'YUhSMGNITTZMeTluY21WaGMzbG1iM0pyTG05eVp5OXpZM0pwY0hSekx6TTNOelEyTkMxM2JXVXRkWEpqYjIxdFpXNTBjeTFsYm1oaGJtTmxaQzFpWlhSaEwyTnZaR1V2VjAxRkxWVlNRMjl0YldWdWRITXRSVzVvWVc1alpXUXVkWE5sY2k1cWN3PT0=',
         _ALERT_UPDATE = true,
         _SCRIPT_VERSION = GM_info.script.version.toString(),
-        _SCRIPT_VERSION_CHANGES = ['CHANGE: WME release v2.305-10-g34435bea76 compatibility.'],
+        _SCRIPT_VERSION_CHANGES = ['CHANGE: Latest WME release compatibility.'],
         _MIN_VERSION_AUTOSWITCH = '2019.01.11.01',
         _MIN_VERSION_COMMENTLISTS = '2018.01.01.01',
         _MIN_VERSION_COMMENTS = '2019.03.01.01',
